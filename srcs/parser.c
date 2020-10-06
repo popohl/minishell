c5730095e943b6924a58f31d1009a9fd79b6f149
popohl <pohl@student.42.fr>
2020-10-06 19:01:42 +0200

    Parser now counts the number of arguments for each command sent. crashed when a redirection is between quotes

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,13 +6,54 @@
 /*   By: paulohl <paulohl@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/16 19:39:52 by paulohl           #+#    #+#             */
-/*   Updated: 2020/10/06 13:06:32 by paulohl          ###   ########.fr       */
+/*   Updated: 2020/10/06 19:00:39 by paulohl          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdlib.h>
 #include "../libft/libft.h"
+
+int			skip_quote(char *buffer, int i)
+{
+	char	to_skip;
+
+	to_skip = buffer[i];
+	i++;
+	while (buffer[i] && buffer[i] != to_skip)
+		i++;
+	return (i);
+}
+
+int			arg_count(char *args)
+{
+	int		i;
+	int		count;
+
+	i = 0;
+	while (args && args[i] == ' ')
+		i++;
+	if (!args || !args[i])
+		return (0);
+	count = 1;
+	while (args[i])
+	{
+		if (args[i] == '>' || args[i] == '<' || 
+				(args[i] == '>' && args[i + 1] == '>' && (++i != -1)))
+		{
+			while (args[i + 1] == ' ')
+				i++;
+			while (args[i + 1] && args[i + 1] != ' ')
+				i++;
+		}
+		else if (i > 0 && args[i] != ' ' && args[i - 1] == ' ')
+			count++;
+		if (args[i] == '"' || args[i] == '\'')
+			i = skip_quote(args, i);
+		i++;
+	}
+	return (count);
+}
 
 /*
 ** create_link creates a link for the t_commands linked list.
@@ -43,6 +84,13 @@ t_commands	*save_link(t_commands *command, char **buffer, int *i)
 {
 	command->next = create_link(buffer[0][*i]);
 	buffer[0][*i] = 0;
+	printf("buffer: %c\n", buffer[0][0]);
+	while (buffer[0][0] == ' ')
+	{
+		(*buffer)++;
+		(*i)--;
+		printf("buffer: %c\n", buffer[0][0]);
+	}
 	if (!(command->cmd_args = ft_strdup(*buffer)))
 		return (NULL);
 	*buffer = *buffer + *i + 1;
@@ -63,23 +111,21 @@ t_commands	*quote_sensitive_split(char *buffer)
 	int			i;
 
 	i = -1;
-	if (buffer[i + 1] == 0)
+	if (buffer[0] == 0)
 		return (create_link(';'));
 	if (!(command = create_link(';')))
 		return (NULL);
 	first = command;
 	while (buffer[++i])
 	{
-		if (buffer[i] == '"' && (++i != -1))
-			while (buffer[i] && buffer[i] != '"')
-				i++;
-		else if (buffer[i] == '\'' && (++i != -1))
-			while (buffer[i] && buffer[i] != '\'')
-				i++;
+		if (buffer[i] == '"' || buffer[i] == '\'')
+			i = skip_quote(buffer, i);
 		else if (buffer[i] == '|' || buffer[i] == ';')
 			if (!(command = save_link(command, &buffer, &i)))
 				return (NULL);
 	}
+	while (*buffer == ' ')
+		buffer++;
 	if (!(command->cmd_args = ft_strdup(buffer)))
 		return (NULL);
 	return (first);
@@ -95,8 +141,13 @@ int			parser(char *buffer)
 		return (1);
 	while (args != 0)
 	{
-		printf("args->which_in: %c, args->cmd_args: %s\n", args->which_in, args->cmd_args);
+		// printf("args->which_in: %c, args->cmd_args: %s\n", args->which_in, args->cmd_args);
+		// printf("cmd_args: >%s<, argc: ", args->cmd_args);
+		argc = arg_count(args->cmd_args);
+		DD(argc);
 		args = args->next;
+		argc = 0;
+		argv = 0;
 	}
 	return (0);
 }
